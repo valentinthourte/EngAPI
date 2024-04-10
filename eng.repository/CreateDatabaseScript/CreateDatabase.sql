2439f2dfda5deb7ca162cb9694adff73c2b35483
vthourte <valentinthourte@gmail.com>
2024-04-10 17:32:02 -0300

    Uploaded CreateDatabase.sql script for database creation

---
+++ eng.repository/CreateDatabaseScript/CreateDatabase.sql
@@ -0,0 +1,17 @@
+IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'EngAPI')
+BEGIN
+	CREATE DATABASE EngAPI
+END
+
+use [dbo.EngAPI]
+
+IF object_id('Users', 'U') is null
+BEGIN
+	CREATE TABLE Users (
+		Id UNIQUEIDENTIFIER,
+		Name varchar(100),
+		Birthdate DATE,
+		Active bit,
+		PRIMARY KEY (Id)
+	)
+END
